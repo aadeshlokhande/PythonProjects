e485df22647e7f6f6afa04c4aa21575ee9ea3f67
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2021-04-27 10:17:32 +0530

    None

--- python automation/unfollow/follow.py
+++ python automation/unfollow/follow.py
@@ -10,13 +10,11 @@ while True:
         pg.click(x,y,interval=1)
         pg.click(int(a),int(b))
         pg.moveTo(x,y)
-        # pg.click(1026,365)
         time.sleep(15)
         print("follow : ",i)
         i+=1
     except:
         pg.moveTo(x,y,1)
         pg.scroll(-4)
-        # pg.press("pagedown")
         time.sleep(1)
         print("Scroll...")
