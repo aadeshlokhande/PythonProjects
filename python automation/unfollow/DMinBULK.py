4671651a78ff7916b7568159fcbc7471464a2cd7
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2021-05-11 04:54:00 +0530

    vb

--- python automation/unfollow/DMinBULK.py
+++ python automation/unfollow/DMinBULK.py
@@ -15,9 +15,11 @@ def gola():
     try:
         x,y = pg.locateCenterOnScreen('gola.png',grayscale=True)
         pg.click(x,y)
-        pg.move(100,0,duration=1)
+        pg.move(100,0,duration=0.5)
     except:
-        pg.hotkey("ctrl","z")
+        pg.hotkey("shift","home")
+        time.sleep(1)
+        pg.press("backspace")
         time.sleep(1)
 
 msg = f"Hey guys...\nDo You want Learn python?\nIf yes then follow : @tech_in_seconds\n"
@@ -27,17 +29,21 @@ pg.click(x=502, y=138,interval=3)      # inbox
 while True:
     for j in range(5):
         pg.click(x=268, y=200,interval=1)  # new msg
-        for i in range(25):
+        pg.typewrite("tec_in_seconds",0.06)
+        time.sleep(2)
+        gola()
+
+        for i in range(10):
             username = list[count]
             count = count + 1
-            pg.typewrite(username, 0.03)  # type usename
-            time.sleep(2)
+            pg.typewrite(username,0.05)  # type usename
+            time.sleep(6)
             gola()
             
 
-        pg.click(x=428, y=152, interval=5)  # next
+        pg.click(x=428, y=152, interval=3)  # next
         pg.click(x=504, y=718)      # click on text input
-        pg.typewrite(msg, interval=0.1)
+        pg.typewrite(msg, interval=0.04)
                 
     pg.click(x=80, y=136,interval=10)       # instagramm
     pg.moveTo(x=343, y=483,duration=5)      # center
--- python automation/unfollow/unfollowInstabot.py
+++ python automation/unfollow/unfollowInstabot.py
@@ -1,17 +1,25 @@
-from instabot import Bot
-from time import sleep
-from random import randint
+# from instabot import Bot
+# from time import sleep
+# from random import randint
 
-bot = Bot()
+# bot = Bot()
 
-bot.login(username="tech_in_seconds", password="CyberInstagram<007>")
+# bot.login(username="tech_in_seconds", password="CyberInstagram<007>")
 
-non_followers = set(bot.following) - set(bot.followers)
+# non_followers = set(bot.following) - set(bot.followers)
 
-for non_follower in non_followers:
-    try:
-        bot.unfollow(non_follower)
-        sleep(randint(6,12))
-    except Exception as e:
-        print(e)
-        sleep(randint(30,150))
+# for non_follower in non_followers:
+#     try:
+#         bot.unfollow(non_follower)
+#         sleep(randint(6,12))
+#     except Exception as e:
+#         print(e)
+#         sleep(randint(30,150))
+
+num = 5
+if (num > 0):
+    print("Number is positive.")
+    # OUTPUT : Number is positive
+    if (num < 10):
+        print("Number is less than 10")
+        # OUTPUT : Number is less than 10
