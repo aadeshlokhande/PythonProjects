17a8cbd66f2d27743061a81e805e27f02881ef56
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2021-05-13 18:28:25 +0530

    javascript variable

---
+++ JavaSript/js/index.js
@@ -0,0 +1,20 @@
+console.log('Aadesh Lokhande');
+// string
+console.log(12+22);
+// calculation
+console.log([12,32,43,54,66]);
+// list/array
+console.log(true);
+//  Boolean value
+console.table({"pooja":5,"aadesh":6})
+// table
+console.log({"pooja":5,"aadesh":6})
+// dic / 
+console.warn("Dont try to open chrome")
+// warning
+console.assert(false,"Not possible")
+// if condition is false. its assertion fail: string will show error on console
+console.clear()
+//  clear the console
+console.error("error is here");
+// showing error
---
+++ JavaSript/js/tut3.js
@@ -0,0 +1,47 @@
+// // VARIABLE
+
+// // 1. VAR
+// var nam = "aadesh"
+// console.log(nam)
+
+// var bhula;
+// // bhula = "Abrakadabra";
+// console.log(bhula)
+
+// var fname = "Aadesh"
+// var lname = "Lokhande"
+// var no = 90;
+// console.log(fname,lname,no)
+// // -----------------------------------------
+
+// 2. const
+// const Name = "Sunny Didi"
+// console.log(Name)
+
+// const Name = "Aadi";
+// Name = "Aadesh";
+// console.log(Name);
+// // Error
+
+// const Name;
+// // Error
+
+// const arr1 = [12,32,43,43];
+// arr1.push(11);
+// console.log(arr1)
+// ------------------------------------------------
+
+// 3. Let
+// {
+//     let city = "Nagpur"
+//     console.log(city)
+// }
+
+var city = "Delhi";
+{
+    let city = "Nagpur";
+    console.log(city)
+    city = "Mumbai";
+    console.log(city)
+}
+console.log(city)
