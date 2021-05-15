99d51ce60a34f29f24130717ce1fd8c465713631
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2021-05-15 05:54:12 +0530

    no

--- JAVASRIPT Classes/jsFile/index.js
+++ JAVASRIPT Classes/jsFile/index.js
@@ -1,5 +1,6 @@
 // console log
 
+console.clear();
 console.log("jethalal");
 console.log(1209);
 console.log(12+20);
@@ -8,7 +9,6 @@ console.log(true);
 console.log(false);
 console.log({"aadesh":23,"pooja":25});
 console.table({"aadesh":23,"pooja":25});
-console.clear();
 console.log([12,23,54,"modi"]);
 console.warn("This is warning");
 console.assert(12<9,"you are wrong");
